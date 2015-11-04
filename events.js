ac4ae79aebfb6395da6dcf0d263ffec8adbe0412
Cosias <Cosias@users.noreply.github.com>
2015-11-04 18:28:07 -0500

    Update events.js
    Method to log to text area

--- events.js
+++ events.js
@@ -3,8 +3,12 @@ var data;
 computeButton.addEventListener("click", function() {
 	          data = document.getElementById("player-data").value;
 	      });
-var player{
+	      
+/*var player{
 	vorp: , //define vorp 
 	salary: , //define salary 
 	position:  // define position 
 };//player object 
+*/
+
+//document.getElementById("results").value + //(some value)  //use this to log to the results text area 
