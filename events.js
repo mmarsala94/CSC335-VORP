46fecf94b4a958ba7cdd3bb6d6055d582259a2ae
darnell1894 <mdarnell@lions.molloy.edu>
2015-11-05 14:59:35 -0500

    Update events.js

--- events.js
+++ events.js
@@ -1,7 +1,7 @@
 var computeButton = document.getElementById("computeButton");
 var data; 
 computeButton.addEventListener("click", function() {
-	          data = document.getElementById("player-data").value;
+	          data = document.getElementById("player-data").string;
 	      });
 
 var splitData = data.split("***");
