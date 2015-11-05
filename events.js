181e5eae7bf3a3a5adc264c7902fdfda4c262143
darnell1894 <mdarnell@lions.molloy.edu>
2015-11-05 14:47:58 -0500

    Update events.js

--- events.js
+++ events.js
@@ -3,8 +3,47 @@ var data;
 computeButton.addEventListener("click", function() {
 	          data = document.getElementById("player-data").value;
 	      });
-var player{
-	vorp: , //define vorp 
-	salary: , //define salary 
-	position:  // define position 
-};//player object 
+
+var splitData = data.split("***");
+
+var arr = splitData[0].split("\n");
+var arr2 = [];
+for(var i = 0; i < arr.length; i++)
+{
+    arr2[i] = arr[i].split("\t");
+}
+
+var arr3 = splitData[1].split("\n");
+var arr4 = [];
+for(var i = 0; i < arr3.length; i++)
+{
+    arr4[i] = arr3[i].split("\t");
+}
+
+var players = {};
+
+function addPlayer(name,position,salary,vorp)
+{
+    players[name]= {
+        Position: position,
+        Salary: salary,
+        VORP : vorp
+    };
+}
+
+for(var i = 0; i < arr2.length; i++)
+{
+    addPlayer(arr2[i][0],arr2[i][2],arr2[i][3], "No Vorp");
+}
+
+for(var i = 0; i < arr4.length; i++)
+{
+    for(var j = 0; j < arr2.length; j++)
+    {
+        if(arr4[i][1] === arr2[j][0])
+        {
+            addPlayer(arr4[i][1], arr2[j][2], arr2[j][3], arr4[i][15]);
+            
+        }
+    }
+}
